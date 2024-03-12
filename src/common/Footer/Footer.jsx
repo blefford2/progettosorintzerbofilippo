2c1b3803f6e5bbe437a1a1504c6c445e6942c49c
unknown <filippofilj01@gmail.com>
2024-03-12 09:08:30 +0100

    Create Footer

---
+++ src/common/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './Footer.css'
+
+const Footer = () => {
+    return(
+        <footer id="footer">
+        <div vclassname= "wrapper">
+        Footer
+        </div>
+        </footer>
+    )
+}
+
+export default Footer
